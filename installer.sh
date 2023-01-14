d7dd9bb4430a70e7b3ef6a7f525e8b3ee7cecc92
AlphaDragon601 <alphadragon601@gmail.com>
2023-01-13 20:37:21 -0500

    prompt and grep reads working

--- installer.sh
+++ installer.sh
@@ -7,12 +7,22 @@ mkdir ~/wrk/
 tar -xf $FILE -C ~/wrk/
 
 echo "Please enter the following info: "
-read -p "name of program: " $prgm_name
-read -p "version number: " $ver_num
-read -p "makefile compiler (default gnu make): " $compiler
+read -p "name of program: " prgm_name
+read -p "version number: " ver_num
+read -p "makefile compiler (default gnu make): " compiler
 compiler=${compiler:-make}
 #echo $compiler
 
 
+searchResult=$( grep -c "\[${prgm_name}\]" ~/programs.conf)
+
+#echo $searchResult
+if [ $searchResult -eq 1 ];then
+    greph -h -w "\[${prgm_name}\]" ~/programs.conf >> $line_nums #find the line number of this program in programs.conf for later
+elif [ $searchResult -gt 1 ]; then
+    echo "${prgm_name} is listed multiple times, check config and try again"
+else
+    read -p "$prgm_name is not installed, would you like to install it? (y/n) " $installYN
+fi
 
 rm -r ~/wrk/
