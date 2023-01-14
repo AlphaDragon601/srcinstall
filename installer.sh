e00a767e6a895e57035dedef8c54d42b214c7db0
AlphaDragon601 <alphadragon601@gmail.com>
2023-01-13 22:01:47 -0500

    install possibly working,problem with make in wsl

--- installer.sh
+++ installer.sh
@@ -9,14 +9,15 @@ tar -xf $FILE -C ~/wrk/
 echo "Please enter the following info: "
 read -p "name of program: " prgm_name
 read -p "version number: " ver_num
-read -p "makefile compiler (default gnu make): " compiler
-compiler=${compiler:-make}
-#echo $compiler
+read -p "build system (default gnu make): " builder
+builder=${builder:-make}
+echo $builder
 
 
 searchResult=$( grep -c "\[${prgm_name}\]" ~/programs.conf)
 
 #echo $searchResult
+
 if [ $searchResult -eq 1 ];then
     greph -h -w "\[${prgm_name}\]" ~/programs.conf >> $line_nums #find the line number of this program in programs.conf for later
 elif [ $searchResult -gt 1 ]; then
@@ -25,4 +26,14 @@ else
     read -p "$prgm_name is not installed, would you like to install it? (y/n) " $installYN
 fi
 
-rm -r ~/wrk/
+if [ "$installYN" = "y" ];then
+    cd ~/wrk/*
+    if [ "$builder" = "make" ];then
+        #make -n
+        ./configure && make && make install
+    fi
+fi
+
+
+
+#rm -r ~/wrk/
