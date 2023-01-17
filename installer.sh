fecb9b668a36e858c83c1b4b371b9ddb17f9a62f
AlphaDragon601 <alphadragon601@gmail.com>
2023-01-16 22:08:29 -0500

    successfully installs, config reading broken

--- installer.sh
+++ installer.sh
@@ -13,27 +13,40 @@ read -p "build system (default gnu make): " builder
 builder=${builder:-make}
 echo $builder
 
+confLoc="/home/carson/programs.conf"
+line_nums=""
 
-searchResult=$( grep -c "\[${prgm_name}\]" ~/programs.conf)
-
-#echo $searchResult
+searchResult=$( grep -c "\[${prgm_name}\]" $confLoc)
+echo "searching $confLoc ..."
+echo "found $prgm_name $searchResult times"
 
 if [ $searchResult -eq 1 ];then
-    greph -h -w "\[${prgm_name}\]" ~/programs.conf >> $line_nums #find the line number of this program in programs.conf for later
+    greph -h -w "\[$prgm_name\]" $confLoc >> $line_nums #find the line number of this program in programs.conf for later
+    echo "found on line $line_nums"
 elif [ $searchResult -gt 1 ]; then
-    echo "${prgm_name} is listed multiple times, check config and try again"
+    echo "$prgm_name is listed multiple times, check config and try again"
 else
-    read -p "$prgm_name is not installed, would you like to install it? (y/n) " $installYN
+    read -p "$prgm_name is not installed, would you like to install it? (y/n) " installYN
+    echo $installYN
 fi
 
+
 if [ "$installYN" = "y" ];then
+    echo "installing..."
+
     cd ~/wrk/*
+    
     if [ "$builder" = "make" ];then
         #make -n
         ./configure && make && make install
     fi
+
+    echo "[$prgm_name]" >> $confLoc
+    echo $ver_num >> $confLoc
+    echo $builder >> $confLoc
+
 fi
 
 
 
-#rm -r ~/wrk/
+rm -r ~/wrk/
