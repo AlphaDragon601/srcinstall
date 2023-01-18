361f16bde10f92324f524f9f48791d2d9606bb66
AlphaDragon601 <alphadragon601@gmail.com>
2023-01-18 17:27:08 -0500

    looks up on conf, moved prompts

--- installer.sh
+++ installer.sh
@@ -8,25 +8,39 @@ tar -xf $FILE -C ~/wrk/
 
 echo "Please enter the following info: "
 read -p "name of program: " prgm_name
-read -p "version number: " ver_num
-read -p "build system (default gnu make): " builder
-builder=${builder:-make}
-echo $builder
+
 
 confLoc="/home/carson/programs.conf"
-line_nums=""
+
 
 searchResult=$( grep -c "\[${prgm_name}\]" $confLoc)
 echo "searching $confLoc ..."
 echo "found $prgm_name $searchResult times"
 
 if [ $searchResult -eq 1 ];then
-    greph -h -w "\[$prgm_name\]" $confLoc >> $line_nums #find the line number of this program in programs.conf for later
+
+    line_nums=$(grep -n "\[$prgm_name\]" $confLoc | cut -d : -f 1)
+
     echo "found on line $line_nums"
+
+    ver_line=$(($line_nums+1))
+    make_line=$(($line_nums+2))
+
+    #echo $ver_line
+    #echo $make_line
+
+
 elif [ $searchResult -gt 1 ]; then
     echo "$prgm_name is listed multiple times, check config and try again"
 else
     read -p "$prgm_name is not installed, would you like to install it? (y/n) " installYN
+
+    read -p "version number: " ver_num
+    read -p "build system (default gnu make): " builder
+    builder=${builder:-make}
+    #echo $builder  
+
+
     echo $installYN
 fi
 
@@ -42,10 +56,12 @@ if [ "$installYN" = "y" ];then
     fi
 
     echo "[$prgm_name]" >> $confLoc
-    echo $ver_num >> $confLoc
+    echo $ver_num >> $confLoc #version number then build tool
     echo $builder >> $confLoc
 
 fi
+
+
 
 
 
