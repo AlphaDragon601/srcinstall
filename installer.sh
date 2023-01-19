77e0dea244b98d8fa7b646a4642fe772041f0df7
AlphaDragon601 <alphadragon601@gmail.com>
2023-01-18 22:16:50 -0500

    added options, deletes but doesn't clear conf yet

--- installer.sh
+++ installer.sh
@@ -1,93 +1,135 @@
 #!/bin/bash
 
-FILE="$1"
 
-mkdir ~/wrk/
-
-tar -xf $FILE -C ~/wrk/
-
-echo "Please enter the following info: "
-read -p "name of program: " prgm_name
-
-
+# change with user
 confLoc="/home/carson/programs.conf"
 TarStoreLoc="/home/carson/storedTars"
 
-searchResult=$( grep -c "\[${prgm_name}\]" $confLoc)
-echo "searching $confLoc ..."
-echo "found $prgm_name $searchResult times"
+
+installFxn() {
+
+    mkdir ~/wrk/
+
+    tar -xzf $FILE -C ~/wrk/
+
+    echo "Please enter the following info: "
+    read -p "name of program: " prgm_name
+
+
+    
+
+    searchResult=$( grep -c "\[${prgm_name}\]" $confLoc)
+    echo "searching $confLoc ..."
+    echo "found $prgm_name $searchResult times"
+
+
+    if [ $searchResult -eq 1 ];then
+
+        line_nums=$(grep -n "\[$prgm_name\]" $confLoc | cut -d : -f 1)
+
+        echo "found on line $line_nums"
+
+        ver_line=$(($line_nums+1))
+        make_line=$(($line_nums+2))
+
+        #echo $ver_line
+        #echo $make_line
+
+        ver_num=$(sed -n ${ver_line}p $confLoc)
+        builder=$(sed -n ${make_line}p $confLoc)
+
+        echo "found version: $ver_num"
+        echo "found build tool: $builder"
+
+
+
+    elif [ $searchResult -gt 1 ]; then
+
+        echo "$prgm_name is listed multiple times, check config and try again"
+
+    else
+
+        read -p "$prgm_name is not installed, would you like to install it? (y/n) " installYN
+
+        read -p "version number: " ver_num
+        read -p "build system (default gnu make): " builder
+        builder=${builder:-make}
+        #echo $builder  
+
+
+        #echo $installYN
+
+        ########installer:###########
+            if [ "$installYN" = "y" ];then
+            echo "installing..."
+
+            cd ~/wrk/*
+            
+            if [ "$builder" = "make" ];then
+                #make -n
+                ./configure && make && make install
+            fi
+
+            cp $FILE $TarStoreLoc #store that tarball for uninstalling later
+
+            echo "[$prgm_name]" >> $confLoc
+            echo $ver_num >> $confLoc #version number then build tool
+            echo $builder >> $confLoc
+            echo $FILE >> $confLoc
+
+        fi
+        #############################
+    fi
 
 
 
 
 
-if [ $searchResult -eq 1 ];then
+    rm -r ~/wrk/
+
+}
+
+
+uninstallFxn() {
 
     line_nums=$(grep -n "\[$prgm_name\]" $confLoc | cut -d : -f 1)
 
     echo "found on line $line_nums"
 
-    ver_line=$(($line_nums+1))
     make_line=$(($line_nums+2))
 
-    #echo $ver_line
-    #echo $make_line
-
-    ver_num=$(sed -n ${ver_line}p $confLoc)
     builder=$(sed -n ${make_line}p $confLoc)
 
-    echo "found version: $ver_num"
     echo "found build tool: $builder"
 
+    path_line=$(($line_nums+3))
 
-
-elif [ $searchResult -gt 1 ]; then
-
-    echo "$prgm_name is listed multiple times, check config and try again"
+    TarName=$(sed -n ${path_line}p $confLoc)
     
-else
+    echo "found tarball name: $TarName"
 
-    read -p "$prgm_name is not installed, would you like to install it? (y/n) " installYN
+    cd $TarStoreLoc
+    tar -xzf $TarName
 
-    read -p "version number: " ver_num
-    read -p "build system (default gnu make): " builder
-    builder=${builder:-make}
-    #echo $builder  
-
-
-    #echo $installYN
-
-    ########installer:###########
-        if [ "$installYN" = "y" ];then
-        echo "installing..."
-
-        cd ~/wrk/*
-        
-        if [ "$builder" = "make" ];then
-            #make -n
-            ./configure && make && make install
-        fi
-
-        cp $FILE $TarStoreLoc #store that tarball for uninstalling later
-
-        echo "[$prgm_name]" >> $confLoc
-        echo $ver_num >> $confLoc #version number then build tool
-        echo $builder >> $confLoc
-
-
+    
+    TarDir=$(echo ${TarName%???????})
+    echo "extracted dir name: $TarDir"
+    cd $TarDir
+    echo "entered"
+    if [ "$builder" = "make" ];then
+    ./configure && make uninstall
     fi
-    #############################
+
+}
+
+
+
+if [ "$1" = "in" ];then
+    FILE=$2
+    installFxn
+elif [ "$1" = "del" ];then
+    prgm_name=$2
+    uninstallFxn
+
+
 fi
-
-
-
-
-
-
-
-
-
-
-
-
-rm -r ~/wrk/
