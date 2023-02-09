9b9bf24a36d0c30395be2db2bfca5216862461de
ProgrammerSockBoi <50785295+AlphaDragon601@users.noreply.github.com>
2023-02-08 20:11:37 -0500

    changed to env variable I forgot existed

--- installer.sh
+++ installer.sh
@@ -2,8 +2,8 @@
 
 
 # change with user
-confLoc="/home/carson/programs.conf" # the location of the config file storing info
-TarStoreLoc="/home/carson/storedTars" # where installed files store tarballs
+confLoc="/home/${USER}/programs.conf" # the location of the config file storing info
+TarStoreLoc="/home/${USER}/storedTars" # where installed files store tarballs
 
 
 
